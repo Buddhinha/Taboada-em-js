10bcb8b378190ba37dbad002137e640d0b9c1007
Arthur <153740966+Buddhinha@users.noreply.github.com>
2024-09-26 14:17:34 -0300

    Create scriptianico.js

---
+++ scriptianico.js
@@ -0,0 +1,72 @@
+
+document.getElementById("numero").value
+console.log(numero)
+//esqueci de verificar os dados, e acabei cometendo um crime
+function verificacao() {
+    var numero= document.getElementById("numero").value
+    console.log(numero)
+    var result = 0
+    var n1 = 1
+    var n2 = 2
+    var n3 = 3
+    var n4 = 4
+    var n5 = 5
+    var n6 = 6
+    var n7 = 7
+    var n8 = 8
+    var n9 = 9
+    var n10 = 10
+    
+    let resultado1 = numero * n1;
+    console.log(resultado1)
+    
+    
+    
+    let resultado2 = numero * n2;
+    console.log(resultado2)
+    
+    
+    let resultado3 = numero * n3;
+    console.log(resultado3)
+    
+    
+    let resultado4 = numero * n4;
+    console.log(resultado4)
+    
+    
+    let resultado5 = numero * n5;
+    console.log(resultado5)
+    
+    
+    let resultado6 = numero * n6;
+    console.log(resultado6)
+    
+    
+    let resultado7 = numero * n7;
+    console.log(resultado7)
+    
+    
+    let resultado8 = numero * n8;
+    console.log(resultado8)
+    
+    
+    let resultado9 = numero * n9;
+    console.log(resultado9)
+    
+    
+    let resultado10 = numero * n10;
+    console.log(resultado10)
+      
+    var res =  document.getElementById('res');
+        res.innerHTML = `${numero} x 1 = ${resultado1}` 
+        res1.innerHTML =`${numero} x 2 = ${resultado2}`
+        res2.innerHTML =`${numero} x 3 = ${resultado3}`
+        res3.innerHTML =`${numero} x 4 = ${resultado4}`
+        res4.innerHTML =`${numero} x 5 = ${resultado5}` 
+        res5.innerHTML =`${numero} x 6 = ${resultado6}`
+        res6.innerHTML =`${numero} x 7 = ${resultado7}`
+        res7.innerHTML =`${numero} x 8 = ${resultado8}`
+        res8.innerHTML =`${numero} x 9 = ${resultado9}`
+        res9.innerHTML =`${numero} x 10 = ${resultado10}`
+
+    }
